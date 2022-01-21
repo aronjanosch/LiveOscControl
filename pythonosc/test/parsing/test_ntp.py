f567745d7a5ecf94c79c3c484e9ce8ba3dbb2617
aronjanosch <aron.wiederkehr@gmail.com>
2022-01-22 00:56:49 +0100

    Added pythonosc package

---
+++ pythonosc/test/parsing/test_ntp.py
@@ -0,0 +1,20 @@
+import unittest
+import time
+
+from pythonosc.parsing import ntp
+
+
+class TestNTP(unittest.TestCase):
+
+    def test_nto_to_system_time(self):
+        unix_time = time.time()
+        timestamp = ntp.system_time_to_ntp(unix_time)
+        unix_time2 = ntp.ntp_to_system_time(timestamp)
+        self.assertTrue(type(unix_time) is float)
+        self.assertTrue(type(timestamp) is bytes)
+        self.assertTrue(type(unix_time2) is float)
+        self.assertAlmostEqual(unix_time, unix_time2, places=5)
+
+
+if __name__ == "__main__":
+    unittest.main()
